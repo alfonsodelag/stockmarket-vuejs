7670c6f9c5f7bd0ffb98b900a199fddb49de07d4
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-03-24 18:36:39 +0100

    adding changes to prices url

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,42 +3,62 @@
     <ul>
 
       <div class="row">
-        <div class="col-3">
+        <div class="col">
           Name
         </div>
-        <div class="col-3">
+        <div class="col">
           Code
         </div>
-        <div class="col-3">
+        <div class="col">
+          Price
+        </div>
+        <div class="col">
           Chart
         </div>
       </div>
+
       <div class="row border" v-for="stock in stocks" v-bind:key="stock.id">
-      <div class="col">
-        <p>{{stock.name}}</p>
-      </div>
-      <div class="col">
-        <p>{{stock.unique_code}}</p>
-      </div>
-      <div class="col">
-        <ul>
-          <li>
-            <img class="chart" src="/src/assets/images/chart.jpg" alt="chart">
-          </li>
-        </ul>
-      </div>
-      <div class="col">
-        <ul>
-            <button class="btn btn-primary">Follow</button>
-        </ul>
-      </div>
-    </div>    
+        <div class="col">
+          <ul>
+            <li>
+              <p>{{stock.actions.name}}</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <ul>
+            <li>
+              <p>{{stock.unique_code}}</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <ul>
+            <li>
+              <p>{{stock.current_quantity}}</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <ul>
+            <li>
+              <img class="chart" src="/src/assets/images/chart.jpg" alt="chart">
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <ul>
+              <button class="btn btn-primary">Follow</button>
+          </ul>
+        </div>
+      </div>    
+      
     </ul>
     
 </template>
 
 <script> 
-const URL = 'https://market-place-laravel.herokuapp.com/api/v1/actions';
+const URL = 'https://market-place-laravel.herokuapp.com/api/v1/prices';
 
 export default {
 	data() {
@@ -71,7 +91,7 @@ export default {
       //document.write(content);
       axios
         .get(
-          `https://market-place-laravel.herokuapp.com/api/v1/actions`
+          `https://market-place-laravel.herokuapp.com/api/v1/prices`
         )
         .then(res => {
                 console.log("then working");
