1751ae4278852304159e5447165786878d2a40bd
Carlos <carlos_agudo@hotmail.com>
2020-03-25 19:37:18 +0100

    routing

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">    
+      <div class="mt-4 collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">List <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Top</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    
+}
+</script>
---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+import Home from '../components/Home';
+
+export default [
+    {path: '/home', component: Home},
+    {path: '/', component: Home},
+    {path: '/top', component: Home},
+    {path: '/search', component: Home},
+    {path: '/detail', component: Home}
+];
